ef613f9631b0be91fce97ffe76025c498de650e8
David R <egoz3ntrum@gmail.com>
2021-03-15 16:24:45 +0100

    ex01

---
+++ 00/ex01/clean.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import datetime
+
+def df_nan_filter(df):
+    """Apply filters on NaN values
+    Args:
+        df: pandas dataframe.
+    Returns:
+        Filtered Dataframe.
+    Raises:
+        This function shouldn't raise any Exception.
+    """
+    df.dropna(subset=['Size'], inplace=True)
+    df['Languages'].fillna(value="EN", inplace=True)
+    df['Price'].fillna(value=0.0, inplace=True)
+    avg = df['Average User Rating'].notna().mean()
+    df['Average User Rating'].fillna(value=avg, inplace=True)
+    df['User Rating Count'].fillna(value=1, inplace=True)
+    return df
+
+
+def change_date_format(date: str):
+    """Change date format from dd/mm/yyyy to yyyy-mm-dd
+    Args:
+        date: a string representing the date.
+    Returns:
+        The date in the format yyyy-mm-dd.
+    Raises:
+        This function shouldn't raise any Exception.
+    """
+    try:
+        newdate = datetime.datetime.strptime(date, '%d/%m/%Y')
+        newdate = newdate.strftime('%Y/%m/%d')
+        return newdate
+    except():
+        return date
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('../appstore_games.csv')
+    clean = df_nan_filter(df)
+    print(df)
